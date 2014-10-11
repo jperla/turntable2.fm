736e893f958e3ea55cdc599d4f9cc1cbb6d3d424
Amelia Arbisser <amarbisser@gmail.com>
2014-10-10 22:31:36 -0400

    creating room from lobby

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,32 +1,35 @@
-$(function(){
-	var source = $("#room-view").html();
-	var dataTemplate = Handlebars.compile(source);
+function bindRoomCreate() {
+	var roomSource = $("#room-view").html();
+	var roomTemplate = Handlebars.compile(roomSource);
 
-	$('#roomcreate').on('keyup', function(e){
+	$('#roomcreate').on('keyup', function(e) {
+		console.log("heard a key on room create");
 		if(e.keyCode === 13) {
 		    var parameters = { room: $(this).val(),
 		                       user: 11 };
 		    $.get( '/create_room', parameters, function(data) {
 			    console.log(data);
-			    $('#room').html(dataTemplate(data));
+			    $('#room').html(roomTemplate(data));
 			    $('#lobby').empty();
 			});
 		};
 	    });
-    });
+    }
 
 $(function(){
-	var source = $("#lobby-view").html();
-	var dataTemplate = Handlebars.compile(source);
+	var lobbySource = $("#lobby-view").html();
+	var lobbyTemplate = Handlebars.compile(lobbySource);
 
 	$('#user').on('keyup', function(e) {
 		if(e.keyCode === 13) {
 		    var parameters = { user: 11 };
 		    $.get('/enter_lobby', parameters, function(data) {
 			    console.log("response from enter lobby: " + data);
-			    $('#lobby').html(dataTemplate(data));
+			    $('#lobby').html(lobbyTemplate(data));
+			    bindRoomCreate();
 			    $('#register').empty();
 			});
 		};
 	    });
+
     });
